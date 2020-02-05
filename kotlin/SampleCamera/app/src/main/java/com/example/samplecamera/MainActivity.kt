75241e29bf0da322f1d5e645960f095f54dfb141
ryouka <coolblossom.0130@gmail.com>
2020-02-06 07:03:15 +0900

    load opencv library

--- kotlin/SampleCamera/app/src/main/java/com/example/samplecamera/MainActivity.kt
+++ kotlin/SampleCamera/app/src/main/java/com/example/samplecamera/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.samplecamera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.opencv.android.CameraActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CameraActivity() {
+
+    init {
+        System.loadLibrary("opencv_java4")
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
